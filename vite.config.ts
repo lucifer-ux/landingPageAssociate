055d2f33720858e3541a23a77da873137a3f8ecf
lucifer-ux <shashwat.cool14@gmail.com>
2026-05-02 00:33:30 +0530

    removing ngrok url

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    allowedHosts: ['multilobed-scoriaceous-zayden.ngrok-free.dev']
-  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
